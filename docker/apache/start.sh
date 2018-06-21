050c31b59af158d51964c3593e77ac631b0e5e07
ssachtleben <ssachtleben@gmail.com>
2018-06-21 13:36:06 +0200

    #2576 added wordpress configuration

--- docker/apache/start.sh
+++ docker/apache/start.sh
@@ -3,9 +3,12 @@
 # Sleep for 30 seconds to wait until mysql is up
 sleep 30
 
-
 # Install Wordpress
-wp core download
+mkdir /var/www/wordpress
+cd /var/www/wordpress
+wp core download --allow-root --quiet
+wp config create --dbhost=wordpress-mysql --dbname=app --dbuser=app --dbpass=app --allow-root --quiet
+wp core install --url=localhost --title=Styla --admin_user=admin --admin_password=admin --admin_email=wecare@styla.com --skip-email --allow-root --quiet
 
 # Starting apache
 apache2-foreground
